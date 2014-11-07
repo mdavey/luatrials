149c096704fad95a5a3c809d5ad1455e74fb0e60
Matthew Davey <github@project-2501.net>
2014-11-07 20:38:37 +1100

    lookup tables are ugly

--- 22_base64.lua
+++ 22_base64.lua
@@ -13,6 +13,50 @@ So many little mistakes, off by one errors with padding
 ]]
 
 
+--[[
+-- Well, it's probably quicker, but not much more simple, readable or shorter
+local a = {}
+local b = {}
+for i=0, 63 do
+    table.insert(a, string.format('[%d]=\'%s\'', i, lookupval(i)))
+    table.insert(b, string.format('[\'%s\']=%d', lookupval(i), i))
+end
+print('local lookupenc = {' .. table.concat(a, ', ') .. '}')
+print('local lookupdec = {' .. table.concat(b, ', ') .. '}')
+
+
+local lookupenc = {[0]='A',  [1]='B',  [2]='C',  [3]='D',  [4]='E',  [5]='F',  [6]='G',  [7]='H', 
+                   [8]='I',  [9]='J',  [10]='K', [11]='L', [12]='M', [13]='N', [14]='O', [15]='P', 
+                   [16]='Q', [17]='R', [18]='S', [19]='T', [20]='U', [21]='V', [22]='W', [23]='X', 
+                   [24]='Y', [25]='Z', [26]='a', [27]='b', [28]='c', [29]='d', [30]='e', [31]='f', 
+                   [32]='g', [33]='h', [34]='i', [35]='j', [36]='k', [37]='l', [38]='m', [39]='n', 
+                   [40]='o', [41]='p', [42]='q', [43]='r', [44]='s', [45]='t', [46]='u', [47]='v', 
+                   [48]='w', [49]='x', [50]='y', [51]='z', [52]='0', [53]='1', [54]='2', [55]='3', 
+                   [56]='4', [57]='5', [58]='6', [59]='7', [60]='8', [61]='9', [62]='+', [63]='/'}
+               
+local lookupdec = {['A']=0,  ['B']=1,  ['C']=2,  ['D']=3,  ['E']=4,  ['F']=5,  ['G']=6,  ['H']=7, 
+                   ['I']=8,  ['J']=9,  ['K']=10, ['L']=11, ['M']=12, ['N']=13, ['O']=14, ['P']=15, 
+                   ['Q']=16, ['R']=17, ['S']=18, ['T']=19, ['U']=20, ['V']=21, ['W']=22, ['X']=23,
+                   ['Y']=24, ['Z']=25, ['a']=26, ['b']=27, ['c']=28, ['d']=29, ['e']=30, ['f']=31, 
+                   ['g']=32, ['h']=33, ['i']=34, ['j']=35, ['k']=36, ['l']=37, ['m']=38, ['n']=39, 
+                   ['o']=40, ['p']=41, ['q']=42, ['r']=43, ['s']=44, ['t']=45, ['u']=46, ['v']=47, 
+                   ['w']=48, ['x']=49, ['y']=50, ['z']=51, ['0']=52, ['1']=53, ['2']=54, ['3']=55, 
+                   ['4']=56, ['5']=57, ['6']=58, ['7']=59, ['8']=60, ['9']=61, ['+']=62, ['/']=63}
+]]
+
+
+-- for encoding
+local function lookupval(v)
+    if v >= 0  and v <= 25 then return string.char(string.byte('A') + v - 0) end
+    if v >= 26 and v <= 51 then return string.char(string.byte('a') + v - 26) end
+    if v >= 52 and v <= 61 then return string.char(string.byte('0') + v - 52) end
+    if v == 62 then return '+' end
+    if v == 63 then return '/' end
+    error('Value: ' .. v .. ' not in range (0-63)')
+end
+
+
+-- for decoding
 local function lookupchar(c)
     b = string.byte(c)
     if b >= string.byte('A') and b <= string.byte('Z') then return b - string.byte('A') + 0  end
@@ -21,16 +65,6 @@ local function lookupchar(c)
     if c == '+' then return 62 end
     if c == '/' then return 63 end
     error('Character: ' .. c .. ' not valid')
-end
-
-
-local function lookupval(v)
-    if v >= 0  and v <= 25 then return string.char(string.byte('A') + v - 0) end
-    if v >= 26 and v <= 51 then return string.char(string.byte('a') + v - 26) end
-    if v >= 52 and v <= 61 then return string.char(string.byte('0') + v - 52) end
-    if v == 62 then return '+' end
-    if v == 63 then return '/' end
-    error('Value: ' .. v .. ' not in range (0-63)')
 end
 
 
@@ -100,16 +134,6 @@ local function decode(s)
     return result
 end
 
-
--- Not sure which is cleaer
-
---[[
-local M = {}
-M.encode = encode
-M.decode = decode
-
-return M
-]]
 
 return {
     encode = encode,
