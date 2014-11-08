9daf0c3eed2014bf6d8ddb951faf49e419eef5fa
Matthew Davey <github@project-2501.net>
2014-11-09 02:14:12 +1100

    at least it doesn't crash now

--- 25_wx_youtubedl_gui.lua
+++ 25_wx_youtubedl_gui.lua
@@ -1,22 +1,71 @@
+-- Description: wx GUI testing async reading from other processes --
+
+--[[
+
+Still crashes when the App exits.  I thought it was process:CloseOutput() not
+being called, but now I'm not sure.
+
+My other crashing problems detailed here:
+http://comments.gmane.org/gmane.comp.lib.wxwidgets.wxlua.user/3177
+
+If these do what I think they do, I'll be annoyed
+wx.wxSafeYield()
+wx.wxWakeUpIdle()
+wx.wxMilliSleep(250)
+]]
+
 
 local wx = require('wx')
 
 local youtubedllocation = [[C:\Users\Ardren\Downloads\youtube-dl.exe]]
 
--- local process = wx.wxProcess(nil, wx.wxPROCESS_REDIRECT)
--- local pid = wx.wxExecute(youtubedllocation, wx.wxEXEC_ASYNC, process)
+--------------------------------------------------------------------------------
 
--- process:Redirect()
+local spawnedprocess = {}
+spawnedprocess.process = nil
+spawnedprocess.pid     = nil
+spawnedprocess.stream  = nil
 
---[[
-   wxProcess *process = new wxProcess(wxPROCESS_REDIRECT);
-   long pid = wxExecute(command, wxEXEC_ASYNC, process);
-   process->Redirect();
-]]
+spawnedprocess.start = function(self, frame, command)
+    self.process = wx.wxProcess(frame, wx.wxPROCESS_REDIRECT)
+    self.process:Redirect()
+    
+    -- Without this (self.process = nil) everything crashes and dies somewhere
+    -- around the last read in ASYNC.  WTF.
+    --
+    -- I think lua is garbage collecting process before the I can even read
+    -- from the input stream (which is now referencing random memory)?
+    self.process:Connect(wx.wxEVT_END_PROCESS, function(event)
+        self.process:CloseOutput()
+        self.process = nil
+    end)
+    
+    self.pid = wx.wxExecute(command, wx.wxEXEC_ASYNC, self.process)        
+    
+    self.stream = self.process:GetInputStream()
+end
 
+spawnedprocess.close = function(self)
+    self.process:CloseOutput()
+end
 
+spawnedprocess.eof = function(self)
+    return self.stream:Eof()
+end
 
+spawnedprocess.canread = function(self)
+    return self.stream:CanRead()
+end
 
+spawnedprocess.read = function(self)
+    local s = ''
+    while self.stream:CanRead() do
+        s = s .. self.stream:Read(1024)
+    end
+    return s
+end
+
+--------------------------------------------------------------------------------
 
 local size  = wx.wxSize(600, 600)
 local title = 'youtube-dl gui'
@@ -30,66 +79,18 @@ sizer:Add(inputtext,  0, wx.wxEXPAND)
 sizer:Add(resulttext, 1, wx.wxEXPAND + wx.wxALL)
 frame:SetSizer(sizer)
 
-local coprocess = nil
-
-local stream
-
-local spawn = function()
-    local command = youtubedllocation .. ' -F ' .. inputtext:GetValue()
-    
-    local process = wx.wxProcess(frame, wx.wxPROCESS_REDIRECT)
-    process:Redirect()
-    
-    local pid = wx.wxExecute(command, wx.wxEXEC_ASYNC, process)    
-    
-    local stream = process:GetInputStream()
-    
-    print('0')
-    
-    while stream:Eof() == false do
-        print('1')
-        if stream:CanRead() then                        
-            print('2')
-            local s = stream:Read(128)            
-            print('3')
-            -- resulttext:AppendText(s)
-            print(s)
-            print('4')
-        end
-        print('5')
-        coroutine.yield()
-        print('6')
-        print(stream)
-        print('7')
-        print(stream, stream:IsOk()) -- wtf, crashes here!
-        print('8')
-        print(stream, stream:Eof()) -- wtf, crashes here!
-        print('9')
-    end
-    
-    print('10')
-    
-    process:CloseOutput()
-    
-    print('11')
-
-end
-
 
 inputtext:Connect(wx.wxEVT_KEY_UP, function(e)
     if e:GetKeyCode() == wx.WXK_RETURN then
-        if coprocess == nil or coroutine.status(coprocess) == "dead" then
-            coprocess = coroutine.create(spawn)        
-        else
-            print('Already running...')
-        end
+        -- spawnedprocess:start(frame, youtubedllocation .. ' -F ' .. inputtext:GetValue())
+        spawnedprocess:start(frame, youtubedllocation .. ' ' .. inputtext:GetValue())
     end
 end)
 
 
 frame:Connect(wx.wxEVT_IDLE, function(e)
-    if coprocess ~= nil and coroutine.status(coprocess) ~= "dead" then
-        local status, msg = coroutine.resume(coprocess)
+    if spawnedprocess.pid ~= nil and spawnedprocess:eof() == false and spawnedprocess:canread() then
+        resulttext:AppendText(spawnedprocess:read())
     end
 end)
 
