224a6f0c2bcabf8e16ad86695cd5ef9b889c4d90
Matthew Davey <github@project-2501.net>
2014-12-14 18:31:06 +1100

    dsl inside functions using envs?
    Bad idea, or new hotness?

---
+++ 30_dsl_by_env.lua
@@ -0,0 +1,36 @@
+-- Description: Silly idea about using fsetebcto make a dsl inside a function --
+
+-- html is just an idea, there's a lot more work to actually make it work
+--  * dont auto close some tags (br, hr)
+--  * always auto close some tags (script)
+--  * first arg should be attributes if table: 
+--    div({style=new}, 'Hello', em('World'))
+--  * how can you write html(function() html(body())) without the second html 
+--    referencing the first?
+--  * indentation !?
+
+local html_dsl_index = function(env, name)
+    return function(...)
+        -- concat all the passed values together
+        local inner = ''
+        for _,value in ipairs({...}) do
+            inner = inner .. value
+        end
+        return '<' .. name .. '>' .. inner .. '</' .. name .. '>'
+    end
+end
+
+local html = function(f)
+    local env = setmetatable({_html = ''}, {__index = html_dsl_index})
+    setfenv(f, env)
+    return f()
+end
+
+----------------------------------------------------------------
+
+local v = html(function()
+    return body(h1('Foo'), h2('Bar'), 'Hello World')
+end)
+
+-- <body><h1>Foo</h1><h2>Bar</h2>Hello World</body>
+print(v)
